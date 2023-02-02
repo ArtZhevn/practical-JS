82ff607e03aaeaddbec80ab9ac6f22ce7682618a
ArtZhevn <113771964+ArtZhevn@users.noreply.github.com>
2023-02-02 19:35:55 +0300

    finish calc form

---
+++ src/js/modules/changeModalState.js
@@ -0,0 +1,48 @@
+import { checkNumInputs } from "./checkNumInputs";
+
+export const changeModalState = (state) => {
+    const windowForms = document.querySelectorAll('.balcon_icons_img');
+    const windowWidths = document.querySelectorAll('#width');
+    const windowHeights = document.querySelectorAll('#height');
+    const windowTypes = document.querySelectorAll('#view_type'); // ?
+    const windowProfiles = document.querySelectorAll('.checkbox');
+
+    checkNumInputs('#width');
+    checkNumInputs('#height');
+
+    const bindActionToElems = (event, elems, prop) => {
+        elems.forEach((elem, i) => {
+            elem.addEventListener(event, () => {
+                switch(elem.nodeName) {
+                    case 'SPAN':
+                        state[prop] = i;
+                        break;
+                    case 'INPUT':
+                        if (elem.getAttribute('type') === 'checkbox') {
+                            i === 0 ? state[prop] = 'Холодное' : state[prop] = 'Тёплое';
+                            elems.forEach((box, j) => {
+                                box.checked = false;
+                                if (i === j) {
+                                    box.checked = true;
+                                }
+                            });
+                        } else {
+                            state[prop] = elem.value;
+                        }
+                        break;
+                    case 'SELECT':
+                        state[prop] = elem.value;
+                        break;
+                }
+
+                console.log(state);
+            });
+        });
+    };
+
+    bindActionToElems('click', windowForms, 'form');
+    bindActionToElems('input', windowHeights, 'height');
+    bindActionToElems('input', windowWidths, 'width');
+    bindActionToElems('change', windowTypes, 'type');
+    bindActionToElems('change', windowProfiles, 'profile');
+};
